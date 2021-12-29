97197a3d20d1156612227d0bddabdb563cf09c1b
James Wang <xingzhewang@gmail.com>
2021-12-29 22:54:17 +0800

    microservice

---
+++ springIni/src/main/java/com/jwln/springIni/SpringIniApplication.java
@@ -0,0 +1,13 @@
+package com.jwln.springIni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIniApplication.class, args);
+	}
+
+}
